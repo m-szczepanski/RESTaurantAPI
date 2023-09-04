1ddc73d75f6ee41b83b8e64d890eb6200be41c3f
Matthew <30538559+m-szczepanski@users.noreply.github.com>
2023-09-04 15:49:32 +0200

    added a class for each model

---
+++ Models/Reservations.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTaurantAPI.Models
+{
+    public class Reservations
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public virtual Calendar Calendar { get; set; }
+        public virtual Table Table { get; set; }
+        public DateTime Hour { get; set; }
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTaurantAPI.Models
+{
+    public class Table
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Seats { get; set; }
+        public bool Available { get; set; }
+        public virtual Calendar Calendar { get; set; }
+    }
+}
