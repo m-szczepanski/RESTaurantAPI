b3a4c0a8c2dd914d71f4925564ba590b453def72
Matthew <30538559+m-szczepanski@users.noreply.github.com>
2023-09-04 16:20:57 +0200

    added DTO objects for every Model

---
+++ DTOs/MenuDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTaurantAPI.DTOs
+{
+    public class MenuDTO
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string DishName { get; set; }
+        public string Quisine { get; set; }
+        public string Ingredients { get; set; }
+        public bool Vegetarian { get; set; }
+        public bool Vegan { get; set; }
+    }
+}
---
+++ DTOs/OrdersDTO.cs
@@ -0,0 +1,17 @@
+﻿using RESTaurantAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RESTaurantAPI.DTOs
+{
+    public class OrdersDTO
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int CalendarId { get; set; }
+        public virtual int TableId { get; set; }
+        public int MenuId { get; set; }
+        public DateTime OrderTime { get; set; }
+        public int Quantity { get; set; }
+    }
+}
