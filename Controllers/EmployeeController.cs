cbedbb13565fa1651812ab082f803e21b985bfb5
Mateusz Szczepański <matiszczep@gmail.com>
2024-01-28 15:46:08 +0100

    feat/employee update functionalities

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -25,7 +25,8 @@ namespace RESTaurantAPI.Controllers
         }
 
         [HttpGet("GetAllEmployees")]
-        public async Task<ActionResult<List<EmployeeDto>>> GetAllEmployees(CancellationToken cancellationToken, int? skip = null, int? limit = null)
+        public async Task<ActionResult<List<EmployeeDto>>> GetAllEmployees(CancellationToken cancellationToken,
+            int? skip = null, int? limit = null)
         {
             var employees = await this.employeeService.GetAllEmployees(cancellationToken);
             var employeesDto = this._mapper.Map<List<EmployeeDto>>(employees);
@@ -44,7 +45,8 @@ namespace RESTaurantAPI.Controllers
         }
 
         [HttpGet("GetEmployeeById/{employeeId}")]
-        public async Task<ActionResult<EmployeeDto>> GetEmployeeById(int employeeId, CancellationToken cancellationToken)
+        public async Task<ActionResult<EmployeeDto>> GetEmployeeById(int employeeId,
+            CancellationToken cancellationToken)
         {
             var employee = await employeeService.GetEmployeeById(employeeId, cancellationToken);
             var employeeDto = this._mapper.Map<EmployeeDto>(employee);
@@ -57,7 +59,8 @@ namespace RESTaurantAPI.Controllers
         }
 
         [HttpGet("GetEmployeesByRole/{role}")]
-        public async Task<ActionResult<List<EmployeeDto>>> GetEmployeesByRole(string role, CancellationToken cancellationToken)
+        public async Task<ActionResult<List<EmployeeDto>>> GetEmployeesByRole(string role,
+            CancellationToken cancellationToken)
         {
             var employee = await employeeService.GetEmployeesByRole(role, cancellationToken);
             var employeeDto = this._mapper.Map<EmployeeDto>(employee);
@@ -70,7 +73,8 @@ namespace RESTaurantAPI.Controllers
         }
 
         [HttpGet("GetEmployeesByLastName/{lastName}")]
-        public async Task<ActionResult<List<EmployeeDto>>> GetEmployeesByLastName(string lastName, CancellationToken cancellationToken)
+        public async Task<ActionResult<List<EmployeeDto>>> GetEmployeesByLastName(string lastName,
+            CancellationToken cancellationToken)
         {
             var employee = await employeeService.GetEmployeesByLastName(lastName, cancellationToken);
             var employeeDto = this._mapper.Map<EmployeeDto>(employee);
@@ -82,6 +86,22 @@ namespace RESTaurantAPI.Controllers
             return Ok(employeeDto);
         }
 
+        [HttpPut("Update/{id}")]
+        public async Task<ActionResult> UpdateEmployee(int id, string firstName, string lastName, string role,
+            CancellationToken cancellationToken)
+        {
+            await employeeService.UpdateEmployee(id, firstName, lastName, role, cancellationToken);
+
+            return Ok("Employee updated successfully.");
+        }
+
+        [HttpPut("UpdateRole/{id}")]
+        public async Task<ActionResult> UpdateRole(int id, string role, CancellationToken cancellationToken)
+        {
+            await employeeService.UpdateRole(id, role, cancellationToken);
+
+            return Ok("Employee's role updated successfully.");
+        }
 
     }
 }
