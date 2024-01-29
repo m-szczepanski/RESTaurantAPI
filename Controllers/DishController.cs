edf398914f7c86e76fb28c058065b3278c489464
Mateusz Szczepański <matiszczep@gmail.com>
2024-01-29 18:18:06 +0100

    feat/dish controller creation

---
+++ Controllers/DishController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RESTaurantAPI.DTOs;
+using RESTaurantAPI.Services;
+
+namespace RESTaurantAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class DishController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly DishService dishService;
+
+        public DishController(DishService dishService, IMapper _mapper)
+        {
+            this._mapper = _mapper;
+            this.dishService = dishService;
+        }
+
+        
+    }
+
+
+}
