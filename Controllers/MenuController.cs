1d537f19eee4029dfa4bf170eb161a200f6b088f
Mateusz Szczepański <matiszczep@gmail.com>
2024-02-07 11:34:48 +0100

    feat/add menu

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RESTaurantAPI.DTOs;
+using RESTaurantAPI.Services;
+
 
 namespace RESTaurantAPI.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        private readonly IMapper _mapper;
+        private readonly MenuService _menuService;
+
+        public MenuController(MenuService menuService, IMapper _mapper)
         {
-            return View();
+            this._mapper = _mapper;
+            this._menuService = menuService;
+        }
+
+        [HttpPost("Add")]
+        public async Task<ActionResult<MenuDto>> AddMenu(List<int> dishIds, string dateString)
+        {
+            var menu = await _menuService.AddMenu(dishIds, dateString);
+
+            var menuDto = _mapper.Map<MenuDto>(menu);
+
+            return Ok(menuDto);
         }
     }
 }
--- Services/MenuService.cs
+++ Services/MenuService.cs
@@ -1,6 +1,40 @@
-﻿namespace RESTaurantAPI.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RESTaurantAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using RESTaurantAPI.Models;
+using RESTaurantAPI.HelpingServices;
+
+
+namespace RESTaurantAPI.Services
 {
     public class MenuService
     {
+        private readonly APIDbContext _dbContext;
+
+        public MenuService(APIDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<Menu> AddMenu(List<int> dishIds, string dateString)
+        {
+            DateOnly date = DateOnly.Parse(dateString);
+            List<Dish> dishes = await _dbContext.Dishes.Where(d => dishIds.Contains(d.Id)).ToListAsync();
+
+            var newMenu = new Menu
+            {
+                Dishes = dishes,
+                Date = date
+            };
+
+            _dbContext.Menus.Add(newMenu);
+            await _dbContext.SaveChangesAsync();
+
+            return newMenu;
+        }
     }
 }
