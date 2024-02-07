c5e1ee02bd60fe76270866b447e610e588455984
Mateusz Szczepański <matiszczep@gmail.com>
2024-02-07 15:06:51 +0100

    feat/order price + migration

--- DTOs/OrderDTO.cs
+++ DTOs/OrderDTO.cs
@@ -9,6 +9,9 @@ namespace RESTaurantAPI.DTOs
         [Key]
         public int Id { get; set; }
 
+        [Required]
+        public decimal OrderPrice { get; set; }
+
         public DateTime OrderTime { get; set; }
         public int Quantity { get; set; }
         public string Status { get; set; }
